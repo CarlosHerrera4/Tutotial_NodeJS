d96390611b6b1720fe7822bf5e7172d7137b0d4e
MundoGister <mundogister@gmail.com>
2016-12-06 19:36:12 +0100

    Changes on listen port

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3002
 
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
 app.use(bodyParser.json())
 
-app.get('/api/product', (req, res) => {
+app.get('/hola', (req, res) => {
 	res.send(200, {products: []})
 })
 
-app.listen(3001, () => {
+app.listen(3002, () => {
 	console.log(`API REST corriendo en http://localhost:${port}`)
 })

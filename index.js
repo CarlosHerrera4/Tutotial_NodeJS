f6e7d0e58b1fb2c53e894b8391e1f032574edaa3
MundoGister <mundogister@gmail.com>
2016-12-10 19:40:43 +0100

    Add GET endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -15,7 +15,7 @@ app.get('/hola', (req, res) => {
 	res.send(200, {products: []})
 })
 
-app.listen(3002, () => {
+app.listen(3001, () => {
 	console.log(`API REST corriendo en http://localhost:${port}`)
-	
+
 })
